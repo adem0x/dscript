53201027e6920ac5aead4753bfd684586318e1c8
coolanmn <coolanmn@50c8f49a-174c-fb25-887c-6828fe759489>
2013-07-09 08:21:57 +0000

    [modify]实现闭包的upvalue。闭包实现不完整。 还缺少函数外部的操作对upvalue的影响。需要一个值复制

--- test/2.lua
+++ test/2.lua
@@ -1,11 +1,11 @@
 add2 = function()
-var i = 1;
-function add(c,d)
-i = c+ d + i;
+var i = 100;
+function add(c)
+i = c+ i;
 return i;
 end;
 return add;
 end;
 c= add2();
-write c(1, 2);
-write c(1, 2);
+write c(1);
+write c(1);
